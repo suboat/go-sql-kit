93fcbcc541a35276f2d89180450a2f65d1aaadd7
wd <wd@suboat.com>
2016-04-21 15:43:11 +0800

    test: order with new rule

--- sql/order_test.go
+++ sql/order_test.go
@@ -3,8 +3,10 @@ package gosql
 import "testing"
 
 func TestSQLOrder_StringJSONtoSQL(t *testing.T) {
+	example := []string{"key1", "+key2", "+key3", "-key4", "-key5"}
 	order := NewSQLOrder()
-	result, err := order.JSONStringsToSQL([]string{"key", "+key_asc", "-key_desc"})
+	order.Allow("key1", "key2", "key5").Disallow("+key3")
+	result, err := order.JSONStringsToSQL(example)
 	if err != nil {
 		t.Fatal(err)
 	}

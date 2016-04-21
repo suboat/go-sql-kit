8b5255a6e5d45866b80e518288d46ad1d2a18627
wd <wd@suboat.com>
2016-04-21 14:03:19 +0800

    test: order sql

--- sql/order_test.go
+++ sql/order_test.go
@@ -1,1 +1,12 @@
 package gosql
+
+import "testing"
+
+func TestSQLOrder_StringJSONtoSQL(t *testing.T) {
+	order := NewSQLOrder()
+	result, err := order.JSONStringsToSQL([]string{"key", "+key_asc", "-key_desc"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(result)
+}

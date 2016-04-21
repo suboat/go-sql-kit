366502bc9611a2e309f7cdf24ab5a3bf0d5bdf20
wd <wd@suboat.com>
2016-04-21 18:16:35 +0800

    test: add query like and error example

--- sql/query_test.go
+++ sql/query_test.go
@@ -75,3 +75,87 @@ func TestSQLQuery_Example3(t *testing.T) {
 	}
 	t.Log(result)
 }
+
+func TestSQLQuery_Example4(t *testing.T) {
+	example := map[string]interface{}{
+		"%and": map[string]interface{}{
+			"%eq": map[string]interface{}{
+				"t1": "111",
+				"t2": 222,
+			},
+			"%ne": map[string]interface{}{
+				"t3": 333,
+				"t4": "444",
+			},
+			"%or": map[string]interface{}{
+				"%lt": map[string]interface{}{
+					"t11": 1111,
+					"t12": "1222",
+				},
+				"%gte": map[string]interface{}{
+					"t13": 1333,
+					"t14": 1444,
+				},
+			},
+			"%like": map[string]interface{}{
+				"t21": 2111,
+				"t22": "2222",
+				"t23": 2333,
+				"t24": 2444,
+			},
+		},
+	}
+	order := NewSQLQuery()
+	order.Allow("t1", "t2", "t3", "t12", "t13", "t21", "t24").Allow("%like")
+	result, err := order.SQLString(example)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(result)
+}
+
+func TestSQLQuery_ErrorExample1(t *testing.T) {
+	example := map[string]interface{}{
+		"%and": map[string]interface{}{
+			"%eq": map[string]interface{}{
+				"t1": "111",
+				"t2": 222,
+			},
+			"%ne": map[string]interface{}{
+				"t3": 333,
+				"t4": "444",
+			},
+			"%or": map[string]interface{}{
+				"%lt": map[string]interface{}{
+					"t11": 1111,
+					"t12": "1222",
+				},
+				"%gte": map[string]interface{}{
+					"%lt": map[string]interface{}{
+						"t11": 1111,
+						"t12": "1222",
+					},
+				},
+			},
+			"%like": map[string]interface{}{
+				"%or": map[string]interface{}{
+					"%lt": map[string]interface{}{
+						"t11": 1111,
+						"t12": "1222",
+					},
+					"%gte": map[string]interface{}{
+						"t13": 1333,
+						"t14": 1444,
+					},
+				},
+			},
+		},
+	}
+	order := NewSQLQuery()
+	order.Allow("t1", "t2", "t3", "t12", "t13").Allow("%like")
+	result, err := order.SQLString(example)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(result)
+}

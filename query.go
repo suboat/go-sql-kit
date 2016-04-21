804163d0f58350cd989cdab41da464947d13fde0
wd <wd@suboat.com>
2016-04-21 16:10:37 +0800

    feat: query impl

--- query.go
+++ query.go
@@ -6,6 +6,14 @@ const (
 	QueryKey1_in         = "%in"
 )
 
+func IsQueryKey1(str string) bool {
+	switch str {
+	case QueryKey1_and, QueryKey1_or, QueryKey1_in:
+		return true
+	}
+	return false
+}
+
 const (
 	QueryKey2_eq   string = "%eq"   // 等于
 	QueryKey2_ne          = "%ne"   // 不等于
@@ -16,27 +24,74 @@ const (
 	QueryKey2_like        = "%like" // 模糊搜索
 )
 
+func IsQueryKey2(str string) bool {
+	switch str {
+	case QueryKey2_eq, QueryKey2_ne:
+		return true
+	case QueryKey2_lt, QueryKey2_lte:
+		return true
+	case QueryKey2_gt, QueryKey2_gte:
+		return true
+	case QueryKey2_like:
+		return true
+	}
+	return false
+}
+
 type IQuery interface {
+	IsAnonymous() bool
 	QueryKey() string
 	QueryValue() []IQuery
-	String() string
 }
 
 type QueryRoot struct {
-	Value IQuery
+	Value []IQuery
 }
 
-func (q *QueryRoot) ParseJSON(str string) error {
-	return nil
+func (q *QueryRoot) IsAnonymous() bool {
+	return false
+}
+
+func (q *QueryRoot) QueryKey() string {
+	return QueryKey1_and
+}
+
+func (q *QueryRoot) QueryValue() []IQuery {
+	return q.Value
 }
 
 type QueryElem struct {
-	Key   string
-	Value IQuery
+	anonymous bool
+	Key       string
+	Value     []IQuery
+}
+
+func (q *QueryElem) IsAnonymous() bool {
+	return q.anonymous
+}
+
+func (q *QueryElem) QueryKey() string {
+	return q.Key
+}
+
+func (q *QueryElem) QueryValue() []IQuery {
+	return q.Value
 }
 
 type QueryValue struct {
 	Key   string
 	Field string
 	Value string
+}
+
+func (q *QueryValue) IsAnonymous() bool {
+	return false
+}
+
+func (q *QueryValue) QueryKey() string {
+	return q.Key
+}
+
+func (q *QueryValue) QueryValue() []IQuery {
+	return nil
 }
--- query_parser.go
+++ query_parser.go
@@ -1,1 +1,59 @@
 package gosql
+
+func (q *QueryRoot) Parse(m map[string]interface{}) error {
+	if m == nil || len(m) == 0 {
+		return nil
+	}
+	q.Value = make([]IQuery, 0, len(m))
+	for k, v := range m {
+		if v == nil {
+			continue
+		} else if IsQueryKey1(k) || IsQueryKey2(k) {
+			elem := &QueryElem{anonymous: IsQueryKey2(k), Key: k}
+			if err := elem.Parse(v); err != nil {
+				continue
+			}
+			q.Value = append(q.Value, elem)
+		}
+	}
+	return nil
+}
+
+func (q *QueryElem) Parse(obj interface{}) error {
+	if m, ok := obj.(map[string]interface{}); !ok {
+		return ErrTypeMap
+	} else if m == nil || len(m) == 0 {
+		return ErrTypeMap
+	} else {
+		q.Value = make([]IQuery, 0, len(m))
+		for k, v := range m {
+			if v == nil {
+				continue
+			} else if IsQueryKey1(k) || IsQueryKey2(k) {
+				elem := &QueryElem{anonymous: IsQueryKey2(k), Key: k}
+				if err := elem.Parse(v); err != nil {
+					continue
+				}
+				q.Value = append(q.Value, elem)
+			} else {
+				value := &QueryValue{Key: q.Key, Field: k}
+				if err := value.Parse(v); err != nil {
+					continue
+				}
+				q.Value = append(q.Value, value)
+			}
+		}
+	}
+	return nil
+}
+
+func (q *QueryValue) Parse(obj interface{}) error {
+	if v, ok := obj.(string); !ok {
+		return ErrTypeString
+	} else if len(v) == 0 {
+		return ErrTypeString
+	} else {
+		q.Value = v
+	}
+	return nil
+}

dd7bfcbd6599e51c38862ea94f91fcb39f820589
WindomZ <zwdsdtc@gmail.com>
2016-04-22 15:06:11 +0800

    feat: init limit rule

--- limit.go
+++ limit.go
@@ -25,3 +25,44 @@ type LimitValue struct {
 func (l *LimitValue) IsLimited() bool {
 	return l.Limit > 0 && (l.Skip+l.Limit*l.Page) >= 0
 }
+
+type IRuleLimit interface {
+	SetDefaultLimit(int) IRuleLimit
+	SetMaxLimit(int) IRuleLimit
+	Limit(int) int
+}
+
+type RuleLimit struct {
+	defaultLimit int
+	maxLimit     int
+}
+
+func (l *RuleLimit) SetDefaultLimit(lmt int) IRuleLimit {
+	if lmt >= 0 {
+		if lmt > l.maxLimit {
+			l.SetMaxLimit(lmt)
+		}
+		l.defaultLimit = lmt
+	}
+	return l
+}
+
+func (l *RuleLimit) SetMaxLimit(lmt int) IRuleLimit {
+	if lmt >= 0 {
+		if lmt < l.defaultLimit {
+			l.SetDefaultLimit(lmt)
+		}
+		l.maxLimit = lmt
+	}
+	return l
+}
+
+func (l *RuleLimit) Limit(lmt int) int {
+	if lmt >= 1 {
+		if lmt > l.maxLimit {
+			return l.maxLimit
+		}
+		return lmt
+	}
+	return l.defaultLimit
+}

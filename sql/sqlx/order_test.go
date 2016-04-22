84c59f2dd9fa2f6aafbf77a917a36a52bfa6647f
WindomZ <zwdsdtc@gmail.com>
2016-04-22 11:38:30 +0800

    test: order and impl

--- sql/sqlx/order_test.go
+++ sql/sqlx/order_test.go
@@ -2,7 +2,18 @@ package gosql
 
 import "testing"
 
-func TestSQLOrder_StringJSONtoSQL(t *testing.T) {
+func TestSQLOrder_ExampleJSON1(t *testing.T) {
+	example := `["key1", "+key2", "+key3", "-key4", "-key5"]` // 定义Example
+	order := NewSQLXOrder()                                   // 初始化
+	order.Allow("key1", "key2", "key5")                       // 设置关键字过滤规则
+	result, err := order.JSONtoSQLString(example)             // 生成SQL语句
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(result)
+}
+
+func TestSQLOrder_Example1(t *testing.T) {
 	example := []string{"key1", "+key2", "+key3", "-key4", "-key5"} // 定义Example，注意前缀设定
 	order := NewSQLXOrder()                                         // 初始化
 	order.Allow("key1", "key2", "key5")                             // 设置关键字过滤规则

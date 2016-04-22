c8c948fd2c35a4547993a891c36aa02dc88ad774
WindomZ <zwdsdtc@gmail.com>
2016-04-22 23:08:48 +0800

    feat: new error

--- error.go
+++ error.go
@@ -4,6 +4,8 @@ import "errors"
 
 var (
 	ErrTypeNil    error = errors.New("gosql: interface{} must not be nil")
-	ErrTypeMap          = errors.New("gosql: interface{} is`t type of map[string]interface{}")
-	ErrTypeString       = errors.New("gosql: interface{} is`t type of string")
+	ErrTypeMap          = errors.New("gosql: interface{} must be type of map[string]interface{}")
+	ErrTypeString       = errors.New("gosql: interface{} must be type of string")
+	ErrTypeInt          = errors.New("gosql: interface{} must be type of int")
+	ErrTypeValue        = errors.New("gosql: invalid value")
 )

9a68251acf157133d417d43fc9c377cbe5332bd3
WindomZ <zwdsdtc@gmail.com>
2016-04-24 20:56:01 +0800

    test: sq; and sqlx

--- sql/order_test.go
+++ sql/order_test.go
@@ -3,10 +3,10 @@ package gosql
 import "testing"
 
 func TestSQLOrder_ExampleJSON1(t *testing.T) {
-	example := `{"%asc":"key1","%desc":"key2"}` // 定义Example
-	order := NewSQLOrder()                      // 初始化
-	order.Allow("key1", "key2")                 // 设置关键字过滤规则
-	sql, err := order.JSONtoSQLString(example)  // 生成SQL语句
+	example := `{"%o":["+key1","-key2"]}`      // 定义Example
+	order := NewSQLOrder()                     // 初始化
+	order.Allow("key1", "key2")                // 设置关键字过滤规则
+	sql, err := order.JSONtoSQLString(example) // 生成SQL语句
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -14,10 +14,10 @@ func TestSQLOrder_ExampleJSON1(t *testing.T) {
 }
 
 func TestSQLOrder_ExampleJSON2(t *testing.T) {
-	example := `{"%asc":"key1","%desc":"key2","%desc":"key5"}` // 定义Example，注意前缀设定
-	order := NewSQLOrder()                                     // 初始化
-	order.Allow("key1", "key2", "key5")                        // 设置关键字过滤规则
-	sql, err := order.JSONtoSQLString(example)                 // 生成SQL语句
+	example := `{"%o":["+key1","-key2","key5"]}` // 定义Example，注意前缀设定
+	order := NewSQLOrder()                       // 初始化
+	order.Allow("key1", "key2", "key5")          // 设置关键字过滤规则
+	sql, err := order.JSONtoSQLString(example)   // 生成SQL语句
 	if err != nil {
 		t.Fatal(err)
 	}
